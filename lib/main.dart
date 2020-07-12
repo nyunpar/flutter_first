130fd29b677a65d87354ab4cd99093b16424d667
nirwan <ny.unpar@gmail.com>
2020-07-12 20:21:08 +0700

    scaffold dan appbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() =>  runApp(MaterialApp(
-  home:Text('hey Cath'),
+  home:Scaffold(
+    appBar:AppBar(
+      title:Text('App Pertama saya'),
+      centerTitle: true,
+    ),
+    body:Center(
+      child:Text('Hallo Cath') ,
+    ),
+    floatingActionButton: FloatingActionButton(
+      onPressed: (){},
+      child:Text("klik")
+    ),
+  ),
 ));
